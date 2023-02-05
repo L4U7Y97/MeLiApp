6a11a314ba58b212acace3f63c2234168aa4948b
L4U7Y97 <pujol760@hotmail.com>
2023-02-04 22:25:34 -0300

    searhbar button

--- src/Components/Searchbar.tsx
+++ src/Components/Searchbar.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
-import { ActionFunctionArgs, Form, Outlet, useNavigate } from 'react-router-dom'
+import { Form, Outlet, useNavigate } from 'react-router-dom'
 import '../Styles/Searchbar.scss'
-
+import { BiSearchAlt2 } from 'react-icons/bi'
 function Searchbar() {
     const navigate = useNavigate()
-    const onSubmit = (event:  React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        navigate({pathname: 'items', search: `search=${event.currentTarget.search.value}` })
+        navigate({ pathname: 'items', search: `search=${event.currentTarget.search.value}` })
     }
     return (
         <>
             <div className='Searchbar'>
-                <Form method='get' onSubmit={onSubmit}>
-                    <input name="search" type="text" />
-                </Form>
+                <div className='Container'>
+                    <Form method='get' onSubmit={onSubmit} className="Form">
+                        <input name="search" type="text" className='Input' />
+                        <button className='Button' type="submit">
+                            <BiSearchAlt2 className='SearchIcon' preserveAspectRatio="xMinYMin" />
+                        </button>
+                    </Form>
+                </div>
             </div>
-            <Outlet />
+            <div className='Container'>
+                <Outlet />
+            </div>
         </>
     )
 }
